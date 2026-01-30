da09530a6b40551b9aeaecb82c3e7ca08b5063e6
Prity kumari <prityykashyap@gmail.com>
2026-01-30 13:31:46 +0530

    Initial commit: semester 2 files

---
+++ semester2/WeatherApp/script.js
@@ -0,0 +1,7 @@
+let input = document.querySelector("#input");
+let btn = document.querySelector("#btn");
+let output = document.querySelector("#output");
+btn.addEventListener("click",()=> {
+    let city = input.value;
+    console.log(city);
+})
